299f35c1549a761e4083dba7f7b3d3abc58f697d
daregoodness1997 <daregoodness@gmail.com>
2025-08-19 00:08:10 +0100

    test passed

--- packages/ui/src/components/Button/Button.test.tsx
+++ packages/ui/src/components/Button/Button.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { test, expect, vi } from 'vitest';
 
-// instead of MantineButton, just mock it so it won't crash
 vi.mock('@mantine/core', () => ({
   Button: (props: any) => <button {...props} />,
 }));
