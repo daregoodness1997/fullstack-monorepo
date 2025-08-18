544df1c67e3abe082e79bae0d163b27a5579a70c
daregoodness1997 <daregoodness@gmail.com>
2025-08-18 09:13:29 +0100

    fixed website build issue

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
  plugins: [react()],
-  resolve: {
-    alias: {
-      react: path.resolve(__dirname, '../../node_modules/react'),
-      'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
-       "react/jsx-runtime": path.resolve(__dirname, "../../node_modules/react/jsx-runtime.js"),
-      "react/jsx-dev-runtime": path.resolve(__dirname, "../../node_modules/react/jsx-dev-runtime.js"),
-    },
-  },
+ 
 })
