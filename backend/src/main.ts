c7a489241be37df5e76c55bb30b2e65cc4fb7915
daregoodness1997 <daregoodness@gmail.com>
2025-08-19 00:15:18 +0100

    added swagger doc to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Fullstack backend repo')
+    .setDescription('The fullstack API description')
+    .setVersion('1.0')
+    .addTag('fullstack')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
